f048f6c1e288beff984742570a7466768d182902
Syed Arsalan Khan <khansyed231am@gmail.com>
2022-09-17 23:12:15 +0500

    updated the app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import { Pet } from "./Pet";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
 render(<App />, document.getElementById("root"));
-
-// React.createElement("div", {}, [
-//   React.createElement("h1", {}, "Adopt Me!"),
-//   React.createElement(Pet, {
-//     name: "Luna",
-//     animal: "Dog",
-//     breed: "Havanese",
-//   }),
-//   React.createElement(Pet, {
-//     name: "Pepper",
-//     animal: "Bird",
-//     breed: "Cocktail",
-//   }),
-//   React.createElement(Pet, {
-//     name: "Doink",
-//     animal: "Cat",
-//     breed: "Mix",
-//   }),
-// ]);
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -18,16 +18,15 @@ export default function useBreedList(animal) {
     async function requestBreedList() {
       setBreedList([]);
       setStatus("loading");
-
       const res = await fetch(
         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
-
       const json = await res.json();
       localCache[animal] = json.breeds || [];
       setBreedList(localCache[animal]);
       setStatus("loaded");
     }
   }, [animal]);
+
   return [breedList, status];
 }
